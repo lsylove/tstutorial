48789e8c8330da5b93c589717aabf2d7b5b49bee
lsylove <20396603+lsylove@users.noreply.github.com>
2018-10-13 17:20:07 +0900

    Added test cases for formatter.py
    - Added test cases for formatter.py
    - Fixed formatter.py to pass test cases

--- test/test_trec_formatter.py
+++ test/test_trec_formatter.py
@@ -5,8 +5,50 @@ from .context import trec
 
 class TC(unittest.TestCase):
     def test_format_attributes(self):
-        line = trec.format_attributes(201, "abcdefg", 1, 0.5)
-        self.assertEqual(line, "".join(["201 Q0 abcdefg 1 0.50 ", definitions.RUN_ID]))
+        line = trec.format_attributes(201, "3.D2M301CF_0449f042", 1, 0.5)
+        expect = "".join(["201 Q0 3.D2M301CF_0449f042 1 0.50", " ", definitions.RUN_ID])
+        self.assertEqual(line, expect)
+
+    def test_format_object(self):
+        obj = {
+            "req_id": 201,
+            "doc_id": "3.D2M301CF_0449f042",
+            "estimate": 0.8
+        }
+        line = trec.format_object(obj, 25)
+        expect = "".join(["201 Q0 3.D2M301CF_0449f042 25 0.80", " ", definitions.RUN_ID])
+        self.assertEqual(line, expect)
+
+    def test_format_object_array(self):
+        lst = [{
+            "req_id": 201,
+            "doc_id": "3.D2M301CF_0449f042",
+            "estimate": 0.8
+        }, {
+            "req_id": 201,
+            "doc_id": "3.D2M301CF_58fb8794",
+            "estimate": 0.5
+        }, {
+            "req_id": 201,
+            "doc_id": "3.D4M301CF",
+            "estimate": 0.
+        }, {
+            "req_id": 201,
+            "doc_id": "3.D4M301CF_0449f042",
+            "estimate": 1.
+        }, {
+            "req_id": 201,
+            "doc_id": "3.D4M301CF_58fb8794",
+            "estimate": 0.1
+        }]
+        formatted = trec.format_object_array(lst)
+        line = "\n".join(formatted)
+        expect = """201 Q0 3.D2M301CF_0449f042 2 0.80 {0}
+201 Q0 3.D2M301CF_58fb8794 3 0.50 {0}
+201 Q0 3.D4M301CF 5 0.00 {0}
+201 Q0 3.D4M301CF_0449f042 1 1.00 {0}
+201 Q0 3.D4M301CF_58fb8794 4 0.10 {0}""".format(definitions.RUN_ID)
+        self.assertEqual(line, expect)
 
 
 if __name__ == "__main__":
