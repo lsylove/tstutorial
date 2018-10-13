2d8f36d7c53f6cc0f5ef6e77cf1ac880bf68b908
lsylove <20396603+lsylove@users.noreply.github.com>
2018-10-13 17:56:00 +0900

    Added trec.docids.Cached class
    - Originally not shown on specifications
    - Added due to (previously) overlooked requirements

--- test/test_trec_docids.py
+++ test/test_trec_docids.py
@@ -23,7 +23,12 @@ class TC(unittest.TestCase):
 
     def test_cached_find(self):
         cached = trec.docids.Cached()
-
+        v = cached.find("3.181515.CMAWYTU0DTIFZWK0MPJBONGCCE43GC2VA_441928e5e7bcf708fa271a09836d2d3f")
+        self.assertEqual(v, "3.181515.CMAWYTU0DTIFZWK0MPJBONGCCE43GC2VA.2")
+        v = cached.find("3.33249.BRG0DNA2QGSIPPXU2OIKA2XISAVKE1XSA_bdd0105d6483489f11ca7c9c4ad0728c")
+        self.assertEqual(v, "3.33249.BRG0DNA2QGSIPPXU2OIKA2XISAVKE1XSA.1")
+        v = cached.find("3.332484.LXYDT0QO41DGZYSWIXPWJPV0PKBKN14FB")
+        self.assertEqual(v, "3.332484.LXYDT0QO41DGZYSWIXPWJPV0PKBKN14FB")
 
 
 if __name__ == "__main__":
--- trec/docids.py
+++ trec/docids.py
@@ -14,3 +14,12 @@ def for_each_doc(func: Callable[[str], Any]) -> None:
     with open(FILE) as file:
         for line in file:
             func(line.split(",")[0])
+
+
+class Cached:
+    def __init__(self):
+        with open(FILE) as file:
+            self.dict = {line.split(",")[0]: line.split(",")[1].strip() for line in file}
+
+    def find(self, doc_id: str) -> str:
+        return self.dict[doc_id]
