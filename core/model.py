fd651f2998ec355c7175f0cd4936e651f94d9759
lsylove <20396603+lsylove@users.noreply.github.com>
2018-11-01 00:09:00 +0900

    Implemented model.py (2)
    - Implemented the rest: layer, loss, training, and evaluate
    - Modified lstm heavily
    - Test cases are now really close to actual implementation
    - TODO: Horrible accuracy which is comparable to a monkey

--- core/model.py
+++ core/model.py
@@ -2,8 +2,48 @@ import tensorflow as tf
 from typing import *
 
 
-def lstm_with_layer_norm(_input: tf.Tensor, internal_dims: int, keep_prob: float=0.8) -> tf.Tensor:
-    lstm = tf.contrib.rnn.LayerNormBasicLSTMCell(internal_dims,
-                                                 dropout_keep_prob=keep_prob)
-    lstm_output, _ = tf.nn.dynamic_rnn(lstm, _input, dtype=tf.float32)
-    return tf.reduce_max(lstm_output, axis=[1])
+def length(_input: tf.Tensor) -> tf.Tensor:
+    used = tf.sign(tf.reduce_max(tf.abs(_input), axis=2))
+    ret = tf.reduce_sum(used, axis=1)
+    ret = tf.cast(ret, tf.int32)
+    return ret
+
+
+def lstm(_input: tf.Tensor, internal_dims: int, keep_prob: tf.Tensor=tf.constant(0.8)) -> tf.Tensor:
+    seq_length = length(_input)
+    basic_lstm = tf.nn.rnn_cell.LSTMCell(internal_dims)
+    dropout_lstm = tf.nn.rnn_cell.DropoutWrapper(basic_lstm, keep_prob, keep_prob)
+    lstm_output, (state_c, state_h) = tf.nn.dynamic_rnn(dropout_lstm, _input,
+                                                        sequence_length=seq_length,
+                                                        dtype=tf.float32)
+    # return tf.reduce_max(lstm_output, axis=[1])
+    return state_h
+
+
+def layer(_input: tf.Tensor, out_shape: int, training_bool: tf.Tensor) -> tf.Tensor:
+    weight_init = tf.random_normal_initializer(stddev=(1.0 / out_shape) ** 0.5)
+    logits = tf.layers.dense(_input, out_shape, kernel_initializer=weight_init)
+    normalized = tf.layers.batch_normalization(logits, momentum=0.9, epsilon=1e-08, training=training_bool)
+    normalized = tf.squeeze(normalized)
+    return tf.nn.sigmoid(normalized)
+
+
+def loss(logits: tf.Tensor, labels: tf.Tensor) -> Tuple[tf.Tensor, tf.Tensor, tf.Tensor]:
+    cross_entropy = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=logits)
+    cost = tf.reduce_mean(cross_entropy)
+    train_loss_summary_op = tf.summary.scalar("train_cost", cost)
+    val_loss_summary_op = tf.summary.scalar("validation_cost", cost)
+    return cost, train_loss_summary_op, val_loss_summary_op
+
+
+def training(cost: tf.Tensor, global_step: tf.Variable, learning_rate: float=0.001) -> tf.Tensor:
+    optimizer = tf.train.AdamOptimizer(learning_rate)
+    return optimizer.minimize(cost, global_step)
+
+
+def evaluate(output: tf.Tensor, labels: tf.Tensor, cond: Callable[[float], bool]) -> Tuple[tf.Tensor, tf.Tensor]:
+    dif = tf.abs(tf.subtract(output, labels))
+    res = tf.map_fn(cond, dif, dtype=tf.bool)
+    accuracy = tf.reduce_mean(tf.cast(res, tf.float32))
+    accuracy_summary_op = tf.summary.scalar("accuracy", accuracy)
+    return accuracy, accuracy_summary_op
--- test/test_core_model.py
+++ test/test_core_model.py
@@ -1,25 +1,29 @@
+import unittest
 import tensorflow as tf
 import numpy as np
+import os
+from typing import *
+from definitions import TEMP_DIR
 from .context import core
 
 
 class TC(tf.test.TestCase):
-    def test_lstm_with_layer_norm(self):
+    def test_lstm(self):
         mock_data = np.random.uniform(-0.25, 0.25, (640, 100, 8)).astype(np.float32)
         dataset = tf.data.Dataset.from_tensor_slices(mock_data)
         dataset = dataset.batch(batch_size=32)
         it = dataset.make_one_shot_iterator()
 
         x = it.get_next()
-        y = core.model.lstm_with_layer_norm(x, 22)
-        init_op = tf.initialize_all_variables()
+        output = core.model.lstm(x, 22)
+        init_op = tf.global_variables_initializer()
 
         with self.test_session() as sess:
             sess.run(init_op)
             ref = [0]
             try:
                 while True:
-                    res = sess.run(y)
+                    res = sess.run(output)
                     ref[0] += 1
                     shape = tf.shape(res)
                     self.assertEqual(shape[0].eval(), 32)
@@ -27,6 +31,94 @@ class TC(tf.test.TestCase):
             except tf.errors.OutOfRangeError:
                 pass
             self.assertEqual(ref[0], 20)
+
+    @unittest.skip
+    def test_loss(self):
+        def gen() -> Iterator[Tuple[np.ndarray, np.float32]]:
+            for idx in range(100):
+                vec = np.random.uniform(-0.25, 0.25, (idx, 64))
+                yield vec, np.add.reduce(np.add.reduce(vec))
+
+        dataset = core.batch.create_dataset(gen, tf.TensorShape([None, 64]))
+        dataset = core.batch.bucket_batch_fixed_size(dataset, 16, [0, 50, 100])
+        it = dataset.make_one_shot_iterator()
+
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+
+        x, y = it.get_next()
+        lstm_output = core.model.lstm(x, 32)
+        output = core.model.layer(lstm_output, 1, tf.constant(True))
+        cost, _, _ = core.model.loss(output, y)
+        train_op = core.model.training(cost, global_step)
+
+        init_op = tf.global_variables_initializer()
+
+        i = 0
+
+        with self.test_session() as sess:
+            sess.run(init_op)
+            try:
+                while True:
+                    _, new_cost = sess.run([train_op, cost])
+                    i += 1
+                    print("Cost at Batch {0}: {1:.4f}".format(i, new_cost))
+            except tf.errors.OutOfRangeError:
+                self.assertEqual(i, 8)
+
+    def test_basic_model(self):
+        def gen() -> Iterator[Tuple[np.ndarray, np.float32]]:
+            for idx in range(1000):
+                vec = np.random.uniform(-0.25, 0.25, (idx // 10, 64))
+                yield vec, 1. if np.add.reduce(np.add.reduce(vec)) > 0 else 0.
+
+        epoch = 200
+
+        dataset = core.batch.create_dataset(gen, tf.TensorShape([None, 64]))
+        val, tra = core.batch.split_dataset(dataset, 150, 100)
+        tra = core.batch.bucket_batch_fixed_size(tra, 128, [0, 50, 100])
+        val = core.batch.bucket_batch_fixed_size(val, 256, [0, 100])
+
+        it = tf.data.Iterator.from_structure(tra.output_types, tra.output_shapes)
+        tra_init_op = it.make_initializer(tra)
+        val_init_op = it.make_initializer(val)
+
+        global_step = tf.Variable(0, name="global_step", trainable=False)
+        training_bool = tf.placeholder(tf.bool)
+        keep_prob = tf.placeholder(tf.float32)
+
+        x, y = it.get_next()
+        lstm_output = core.model.lstm(x, 32, keep_prob=keep_prob)
+        output = core.model.layer(lstm_output, 1, training_bool)
+        cost, train_loss_summary_op, val_loss_summary_op = core.model.loss(logits=output, labels=y)
+        accuracy, accuracy_summary_op = core.model.evaluate(output, y, lambda v: v < 0.25)
+
+        train_op = core.model.training(cost, global_step)
+        extra_update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+
+        init_op = tf.global_variables_initializer()
+
+        with self.test_session() as sess:
+            log_dir = os.path.join(TEMP_DIR, "tb")
+            summary_writer = tf.summary.FileWriter(log_dir, graph=sess.graph)
+
+            sess.run(init_op)
+            for i in range(epoch):
+                print("Epoch #", i + 1)
+                sess.run(tra_init_op)
+                while True:
+                    try:
+                        _, _, train_summary = sess.run([train_op, extra_update_ops, train_loss_summary_op],
+                                                       feed_dict={training_bool: True,
+                                                                  keep_prob: 0.8})
+                        summary_writer.add_summary(train_summary, sess.run(global_step))
+                    except tf.errors.OutOfRangeError:
+                        sess.run(val_init_op)
+                        val_summary, val_loss_summary = sess.run([accuracy_summary_op, val_loss_summary_op],
+                                                                 feed_dict={training_bool: False,
+                                                                            keep_prob: 1.})
+                        summary_writer.add_summary(val_summary, sess.run(global_step))
+                        summary_writer.add_summary(val_loss_summary, sess.run(global_step))
+                        break
 
 
 if __name__ == "__main__":
